86ecdd1012822392b3b3b74e0867ac88517007a6
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-02-13 14:48:42 +0530

    Making changes

--- Class-02/Deletion/delete.js
+++ Class-02/Deletion/delete.js
@@ -1,25 +1,26 @@
-const input = document.querySelector('#input');
-const button = document.querySelector('#submit')
-const defaultDisplay = document.querySelector('#defaultDisplay')
-const display = document.querySelector('#display')
+const input = document.querySelector("#input");
+const button = document.querySelector("#submit");
+const defaultDisplay = document.querySelector("#defaultDisplay");
+const display = document.querySelector("#display");
 
-
-const marks = [91,95,89,81,85,75];
+const marks = [91, 95, 89, 81, 85, 75];
 defaultDisplay.innerHTML = `Item list : ${marks} :: Input should be less than total number of display Item. {Negative Integer is not allowed}`;
 
+//Validation is important without this it creates some problems. So, be careful about Validation
+
 const delFunction = () => {
-    const delPosition = parseInt(input.value);
+  const delPosition = parseInt(input.value);
 
-    if(marks.length == 0){
-        alert('List is empty');
-    }
+  if (marks.length == 0) {
+    alert("List is empty");
+  }
 
-    for(let i = delPosition; i < (marks.length - 1); i++){
-        marks[i] = marks [i + 1]
-    }
-    marks.length = (marks.length - 1);
+  for (let i = delPosition; i < marks.length - 1; i++) {
+    marks[i] = marks[i + 1];
+  }
+  marks.length = marks.length - 1;
 
-    display.innerHTML = `Now, The Remain items : ${marks}`;
-}
+  display.innerHTML = `Now, The Remain items : ${marks}`;
+};
 
-button.addEventListener('click', delFunction);
+button.addEventListener("click", delFunction);
